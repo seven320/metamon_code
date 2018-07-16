15d5909d2cf6e80e5cbb392b81afb2c5c912c7b3
seven320 <yosyuaomenw@yahoo.co.jp>
2018-07-17 02:33:17 +0900

    mecab いじった

--- mecab.py
+++ mecab.py
@@ -19,35 +19,46 @@ def NLP(text):#natural language processing
     # print(m.parse(text))
     return m.parse(text)
 
-def sprit_text_to_noun(text,frequency_dic):
+def sprit_text_to_noun(text):
+    frequency_dic = {}
+    frequency_verb = {}
     m = MeCab.Tagger("-Ochasen -d /usr/local/lib/mecab/dic/mecab-ipadic-neologd")
     #自然言語処理
-    node=m.parse(text)
+    node = m.parse(text)
 
-    pos=node.split("\n")#単語ごとに切ってリストに格納
+    pos = node.split("\n")#単語ごとに切ってリストに格納
     for i in range(len(pos)):
         if "名詞" in pos[i]:#名詞だけ抽出
-            print(pos[i])
-            noun=pos[i].split("\t")[0]
+            # print(pos[i])
+            noun = pos[i].split("\t")[0]
             if noun in frequency_dic.keys():
                 frequency_dic[noun]+=1
             else:
                 frequency_dic.update({noun:1})
-    print(frequency_dic)
+
+        if "動詞" in pos[i]:#動詞だけ抽出
+            print(pos[i])
+            verb = pos[i].split("\t")[0]
+            if verb in frequency_verb.keys():
+                frequency_verb[verb]+=1
+            else:
+                frequency_verb.update({verb:1})
+    print("名詞一覧：",frequency_dic)
+    print("動詞一覧：",frequency_verb)
 
 def main():
-    auth=account.Initialize()
+    auth = account.Initialize()
     api = tweepy.API(auth)
     twitter_id=account.id()
 
-    public_tweets = api.home_timeline(count=100)
+    public_tweets = api.home_timeline(count=1)
 
-    frequency_dic={}
+
     for tweet in public_tweets:
         print("\n"+tweet.user.name)
         # print(tweet.user.screen_name)#@以下のID
         print(tweet.text)
-        print(sprit_text_to_noun(tweet.text,frequency_dic))
+        print(sprit_text_to_noun(tweet.text))
 
 if __name__=="__main__":
     # sprit_text_to_noun(text)
--- meta_manuscript.py
+++ meta_manuscript.py
@@ -3,13 +3,13 @@ class Manuscript():
     def __init__(self):
         self.tweet=[]
         #1~
-        self.tweet.append("仕事終わらせてえらい！！")
+        self.tweet.append("仕事終わらせてえらいもん！！")
         self.tweet.append("よく頑張りました！！！")
         self.tweet.append("満点！")
         self.tweet.append("いつも最高です。")
-        self.tweet.append("とても優秀！")
+        self.tweet.append("とても優秀だもん！")
         self.tweet.append("よっ、社長！！")
-        self.tweet.append("できる人は違うね")
+        self.tweet.append("できる人は違うもん")
         self.tweet.append("頑張り屋さんです")
         self.tweet.append("すごい！\nとてもすごい！！")
         self.tweet.append("すばらしい！")
@@ -18,9 +18,9 @@ class Manuscript():
         self.tweet.append("今日も頑張ってます")
         self.tweet.append("今日の成果も素晴らしい！！")
         self.tweet.append("わんだふる！！！")
-        self.tweet.append("素晴らしい成果です！！")
+        self.tweet.append("素晴らしい成果だもん！！")
         self.tweet.append("素晴らしい出来栄えです")
-        self.tweet.append("仕事を頑張る姿が素敵です!!”)
+        self.tweet.append("仕事を頑張る姿が素敵もん!!”)
 
     def manus(self,num=0):
         return self.tweet[num]

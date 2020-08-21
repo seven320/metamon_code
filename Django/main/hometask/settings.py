69da37032bb9fefe3eec0792dc88e887e61f6577
seven320 <yosyuaomenw@yahoo.co.jp>
2020-08-22 01:57:55 +0900

    rm key

--- Django/main/hometask/settings.py
+++ Django/main/hometask/settings.py
@@ -16,6 +16,7 @@ try:
     from .local_settings import *
 except ImportError:
     pass
+
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 

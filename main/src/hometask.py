65665dd1cbdb070eb6802d58e14a4f6481615be4
seven320 <yosyuaomenw@yahoo.co.jp>
2020-09-21 15:34:28 +0900

    make hometask crontab

--- main/src/hometask.py
+++ main/src/hometask.py
@@ -15,134 +15,7 @@ from src import meta_manuscript
 from src import hometamon
 from src import tweet_intent
 
-class HomeTask(hometamon.Hometamon):
-    def __init__(self):
-        super().__init__()
-        self.task_words = ["#hometask"]
-        self.django_api = API()
 
-    def check_task(self, tweet):
-        if not "@denden_by" in tweet.text:
-            return False
-        for set_task_word in self.set_task_words:
-            if set_task_word in tweet.text:
-                return True
-        return False
-
-    def check_task_history(self, tweet):
-        if "#hometask" in tweet.text:
-            return True
-        return False
-
-    def extract_task(self, tweet_text):
-        task = tweet_text.replace("@denden_by","").replace("\n", "").replace(" ", "").replace(":", "").replace("：", "")
-        for set_task_word in self.set_task_words:
-            task = task.replace(set_task_word, "")
-        return task
-
-    def set_task_and_reply(self, tweet):
-        task = self.extract_task(tweet.text)
-        if self.hometask_api.set_task(tweet.user, task):
-            intent_url = tweet_intent.make(text = "", hashtag = "hometask")
-            reply = "@" + tweet.user.screen_name + "\n" + "{}を覚えたもん！今日から頑張るもん!!\n報告は #hometask をつけてもん!!\n{}\n".format(task, intent_url)
-        else:
-            reply = "@" + tweet.user.screen_name + "\n" + "うまく覚えれなかったもん\nごめんなさいもん"
-        self.api.update_status(status = reply, in_reply_to_status_id = tweet.id)
-        self.api.create_favorite(tweet.id)
-        return reply
-
-    def set_task_history_and_reply(self, tweet):
-        if self.hometask_api.set_task_history(tweet.id, tweet.text, tweet.user.id):
-            reply = hometask.make_reply(user_id = tweet.user.id)
-        else:
-            reply = "まだtaskが設定されてないもん!!下のurlからツイートして設定するもん."
-            reply = reply + "\n" + tweet_intent.make(text = "settask:", tweet_to="denden_by")
-        reply = "@" + tweet.user.screen_name + "\n" + self.user_name_changer(tweet) + reply
-        self.api.update_status(status = reply, in_reply_to_status_id = tweet.id)
-        self.api.create_favorite(tweet.id)
-        return reply
-
-    def classify_reply(self, user_id):
-        # user_idに基づくので，taskを変更してもしなくても関係ない．
-        r = self.django_api.search_task_history(user_id)
-        if r.status_code != 200:
-            return "", None
-        count = r.json()["count"]
-        task_historys = r.json()["results"]
-        streak_count = count_hometask_streak(task_historys)
-        if streak_count in list(self.manuscript.streak_reply.keys()):
-            return self.praised_streak(streak_count), count
-        if count in list(self.manuscript.icon.keys()):
-            return self.praised_count_hometask(count), count
-        return self.praised_random(), count
-
-    def make_icon(self, count):
-        icons = self.manuscript.icon
-        icon = ""
-        for key in icons.keys():
-            if count >= key:
-                icon = icons[key]
-        return chr(int(icon, 16))
-
-    def make_reply(self, user_id):
-        reply, count = self.classify_reply(user_id)
-        if count == None:
-            print("count is None")
-        reply = make_icon(count) + reply
-        return reply 
-
-    # 各種のreply
-    def praised_random(self):
-        reply = random.choice(self.manuscript.hometask_random_reply)
-        return reply
-
-    def praised_count_hometask(self, count):
-        reply = self.manuscript.count_reply[count]
-        reply = reply + self.make_icon(count)
-        return reply
-
-    def praised_streak(self, streak_count):
-        reply = self.manuscript.streak_reply[streak_count]
-        return reply    
-    
-    def count_hometask_streak(self, task_historys):
-        # 現在のstreakを計算する
-        reversed_task_historys = list(reversed(task_historys))
-        streak_count = 0
-        newest_created_at = dt.strptime(reversed_task_historys[0]["created_at"],"%Y-%m-%dT%H:%M:%S.%f").date()
-        pre_created_at = newest_created_at
-        for i, task in enumerate(reversed_task_historys):
-            created_at = dt.strptime(task["created_at"], "%Y-%m-%dT%H:%M:%S.%f")
-            if pre_created_at - datetime.timedelta(days=1) > created_at.date():
-                break
-            pre_created_at = created_at.date()
-        streak_count = (newest_created_at - pre_created_at).days + 1
-        return streak_count
-
-    def hometask_exclude(self, tweet):
-        if str(tweet.user.id) == self.my_twitter_user_id:
-            return True
-        elif tweet.favorited:
-            return True
-        elif tweet.text.split(" ")[0] == "RT":
-            return True
-        elif tweet.text.split(" ")[0][0] == "@":
-            if "@denden_by" in tweet.text:
-                self.api.create_favorite(id = tweet.id)
-            return True
-        return False  
-
-def main():
-    hometask = HomeTask()
-    public_tweets = hometask.get_tweets()
-    for tweet in public_tweets:
-        if hometask.hometask_exclude(tweet):
-            pass
-        else:
-            if hometask.check_task(tweet): # set taskをする
-                reply = set_task_and_reply(tweet)
-            elif hometask.check_task_history(tweet): # hometask
-                reply = set_task_history_and_reply(tweet)
 
 """
 DjangoのサーバーとCRUDを行う．
@@ -200,8 +73,6 @@ class API():
         url = self.base_url + "/task_historys/"
 
         response = requests.post(url, data = json.dumps(data), headers = headers)
-        print(response.json)
-        print(response.content)
         return response
 
     def search_task(self, user_id):
@@ -212,8 +83,6 @@ class API():
     def search_task_history(self, user_id):
         url = self.base_url + "/search/task_historys/?user_id={}".format(user_id)
         response = requests.get(url)
-        print(response.json)
-        print(response.content)
         return response
 
     def set_user(self, user):
@@ -264,3 +133,135 @@ class API():
         if r.status_code != 201:
             return False
         return True
+
+class HomeTask(hometamon.Hometamon):
+    def __init__(self):
+        super().__init__()
+        self.task_words = ["#hometask"]
+        self.django_api = API()
+
+    def check_task(self, tweet):
+        if not "@denden_by" in tweet.text:
+            return False
+        for set_task_word in self.set_task_words:
+            if set_task_word in tweet.text:
+                return True
+        return False
+
+    def check_task_history(self, tweet):
+        if "#hometask" in tweet.text:
+            return True
+        return False
+
+    def extract_task(self, tweet_text):
+        task = tweet_text.replace("@denden_by","").replace("\n", "").replace(" ", "").replace(":", "").replace("：", "")
+        for set_task_word in self.set_task_words:
+            task = task.replace(set_task_word, "")
+        return task
+
+    def set_task_and_reply(self, tweet):
+        task = self.extract_task(tweet.text)
+        if self.django_api.set_task(tweet.user, task):
+            intent_url = tweet_intent.make(text = "", hashtag = "hometask")
+            reply = "@" + tweet.user.screen_name + "\n" + "{}を覚えたもん！今日から頑張るもん!!\n報告は #hometask をつけてもん!!\n{}\n".format(task, intent_url)
+        else:
+            reply = "@" + tweet.user.screen_name + "\n" + "うまく覚えれなかったもん\nごめんなさいもん"
+        self.api.update_status(status = reply, in_reply_to_status_id = tweet.id)
+        self.api.create_favorite(tweet.id)
+        return reply
+
+    def set_task_history_and_reply(self, tweet):
+        if self.django_api.set_task_history(tweet.id, tweet.text, tweet.user.id):
+            reply = self.make_reply(user_id = tweet.user.id)
+        else:
+            reply = "まだtaskが設定されてないもん!!下のurlからツイートして設定するもん."
+            reply = reply + "\n" + tweet_intent.make(text = "settask:", tweet_to="denden_by")
+        reply = "@" + tweet.user.screen_name + "\n" + self.user_name_changer(tweet) + reply
+        self.api.update_status(status = reply, in_reply_to_status_id = tweet.id)
+        self.api.create_favorite(tweet.id)
+        return reply
+
+    def classify_reply(self, user_id):
+        # user_idに基づくので，taskを変更してもしなくても関係ない．
+        r = self.django_api.search_task_history(user_id)
+        if r.status_code != 200:
+            return "", None
+        count = r.json()["count"]
+        task_historys = r.json()["results"]
+        streak_count = self.count_hometask_streak(task_historys)
+        if streak_count in list(self.manuscript.streak_reply.keys()):
+            return self.praised_streak(streak_count), count
+        if count in list(self.manuscript.icon.keys()):
+            return self.praised_count_hometask(count), count
+        return self.praised_random(), count
+
+    def make_icon(self, count):
+        icons = self.manuscript.icon
+        icon = ""
+        for key in icons.keys():
+            if count >= key:
+                icon = icons[key]
+        return chr(int(icon, 16))
+
+    def make_reply(self, user_id):
+        reply, count = self.classify_reply(user_id)
+        if count == None:
+            print("count is None")
+        reply = self.make_icon(count) + reply
+        return reply 
+
+    # 各種のreply
+    def praised_random(self):
+        reply = random.choice(self.manuscript.hometask_random_reply)
+        return reply
+
+    def praised_count_hometask(self, count):
+        reply = self.manuscript.count_reply[count]
+        reply = reply + self.make_icon(count)
+        return reply
+
+    def praised_streak(self, streak_count):
+        reply = self.manuscript.streak_reply[streak_count]
+        return reply    
+    
+    def count_hometask_streak(self, task_historys):
+        # 現在のstreakを計算する
+        reversed_task_historys = list(reversed(task_historys))
+        streak_count = 0
+        newest_created_at = dt.strptime(reversed_task_historys[0]["created_at"],"%Y-%m-%dT%H:%M:%S.%f").date()
+        pre_created_at = newest_created_at
+        for i, task in enumerate(reversed_task_historys):
+            created_at = dt.strptime(task["created_at"], "%Y-%m-%dT%H:%M:%S.%f")
+            if pre_created_at - datetime.timedelta(days=1) > created_at.date():
+                break
+            pre_created_at = created_at.date()
+        streak_count = (newest_created_at - pre_created_at).days + 1
+        return streak_count
+
+    def hometask_exclude(self, tweet):
+        if str(tweet.user.id) == self.my_twitter_user_id:
+            return True
+        elif tweet.favorited:
+            return True
+        elif tweet.text.split(" ")[0] == "RT":
+            return True
+        elif tweet.text.split(" ")[0][0] == "@":
+            if "@denden_by" in tweet.text:
+                self.api.create_favorite(id = tweet.id)
+            return True
+        return False  
+
+def main():
+    ht = HomeTask()
+    public_tweets = ht.get_tweets()
+    for tweet in public_tweets:
+        if ht.hometask_exclude(tweet):
+            pass
+        else:
+            if ht.check_task(tweet): # set taskをする
+                reply = ht.set_task_and_reply(tweet)
+            elif ht.check_task_history(tweet): # hometask
+                reply = ht.set_task_history_and_reply(tweet)
+
+if __name__ == "__main__":
+    main()
